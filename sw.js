dd054bfff3688ad8912d8d9c68179b2c1b72ce2d
vf12776-ux <vf12776-ux@users.noreply.github.com>
2026-08-11 23:30:20 +0300

    Initial commit: Rustar Jackets PWA

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'rustar-hub-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './style.css',
+  './icon-192.png',
+  './offline.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) return response;
+      return fetch(event.request).then(response => {
+        if (!response || response.status !== 200 || response.type !== 'basic') return response;
+        const responseToCache = response.clone();
+        caches.open(CACHE_NAME).then(cache => { cache.put(event.request, responseToCache); });
+        return response;
+      }).catch(() => caches.match('./offline.html'));
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
